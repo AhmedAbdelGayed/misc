c746188b6f94b20a10a6c828353619d841f50391
Ahmed Mohamed <ahmed.abd.elgayed@gmail.com>
2018-03-12 22:05:51 -0700

    Update CorrelationMatrix.py

--- CorrelationMatrix.py
+++ CorrelationMatrix.py
@@ -1,7 +1,6 @@
-#Defination: This Python function builds an item to item collaboration filering based recommendations based on a correlation matrix which can be used for lookups.
-#The matrix values are correlation values between values. The values are calculated based on pandas corr function that can be called using multile values and methods to calculate correlation
-#The input is a dataframe of users and products/Services usage
-#Application: The application of the function can be used to determine somehow the correlation between the products/services based on customer usage. It is useful to determine how a specific product/service change can impact other products 
+#Defination: This Python function builds an item to item collaboration filering based recommendations engine based on a correlation matrix which is used basically for lookups. The matrix values are correlation values between items. The values are calculated based on pandas corr function that can be called using multile correlation methods to calculate correlation values with minimum events.
+#Input: is a Pandas dataframe of users and Products/Services usage.
+#Application: The application of the function can be used to determine the correlation between the products/services based on customer usage. It is useful to determine how a specific product/service change can impact other products too.
 import os 
 import pyodbc
 import pandas as pd 
